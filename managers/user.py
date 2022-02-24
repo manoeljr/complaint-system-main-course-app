100a8beb23a9c2a8651437e23bd6bf562955606a
Manoeljr <manoelv8@gmail.com>
2022-02-24 16:37:04 -0300

    Criando a rota de registro de usuario que retorna o token de acesso

---
+++ managers/user.py
@@ -0,0 +1,23 @@
+from asyncpg import UniqueViolationError
+from fastapi import HTTPException
+from passlib.context import CryptContext
+
+from db import database
+from managers.auth import AuthManager
+from models import user
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class UserManager:
+
+    @staticmethod
+    async def register(user_data):
+        user_data["password"] = pwd_context.hash(user_data["password"])
+        try:
+            id_ = await database.execute(user.insert().value(**user_data))
+        except UniqueViolationError:
+            raise HTTPException(400, "User with this email already exists")
+
+        user_new = await database.fetch_one(user.select().where(user.c.id == id_))
+        return AuthManager.encode_token(user_new)
