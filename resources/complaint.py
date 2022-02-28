d5f8e6c361b1d34fde520d844bb7651733b47085
Manoeljr <manoelv8@gmail.com>
2022-02-28 11:04:50 -0300

    Adicionando a rota delete complaints

--- resources/complaint.py
+++ resources/complaint.py
@@ -3,8 +3,9 @@ from typing import List
 from fastapi import APIRouter, Depends
 from starlette.requests import Request
 
-from managers.auth import oauth_scheme, is_complainer
+from managers.auth import oauth_scheme, is_complainer, is_admin
 from managers.complaint import ComplaintManager
+from schemas.request.complaint import ComplaintIn
 from schemas.response.complaint import ComplaintOut
 
 router = APIRouter(tags=["Complaints"])
@@ -13,7 +14,8 @@ router = APIRouter(tags=["Complaints"])
 @router.get(
     "/complaints/",
     dependencies=[Depends(oauth_scheme)],
-    response_model=List[ComplaintOut]
+    response_model=List[ComplaintOut],
+    status_code=200
 )
 async def get_complaints(request: Request):
     user = request.state.user
@@ -23,8 +25,19 @@ async def get_complaints(request: Request):
 @router.post(
     "/complaints/",
     dependencies=[Depends(oauth_scheme), Depends(is_complainer)],
-    response_model=ComplaintOut
+    response_model=ComplaintOut,
+    status_code=201
 )
-async def create_complaint(complaint: ComplaintOut):
-    return await ComplaintManager.create_complaint(complaint.dict())
+async def create_complaint(request: Request, complaint: ComplaintIn):
+    user = request.state.user
+    return await ComplaintManager.create_complaint(complaint.dict(), user)
+
+
+@router.delete(
+    "/complaints/{complaint_id}/",
+    dependencies=[Depends(oauth_scheme), Depends(is_admin)],
+    status_code=204
+)
+async def delete_complaint(complaint_id: int):
+    await ComplaintManager.delete_complaint(complaint_id)
 
