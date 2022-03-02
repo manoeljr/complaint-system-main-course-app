17534bd1305908b15a5dae2733597f8c434f447b
Manoeljr <manoelv8@gmail.com>
2022-03-02 18:21:16 -0300

    Criando rota de atualização dos complaints

--- resources/complaint.py
+++ resources/complaint.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 from starlette.requests import Request
 
-from managers.auth import oauth_scheme, is_complainer, is_admin
+from managers.auth import oauth_scheme, is_complainer, is_admin, is_approver
 from managers.complaint import ComplaintManager
 from schemas.request.complaint import ComplaintIn
 from schemas.response.complaint import ComplaintOut
@@ -41,3 +41,20 @@ async def create_complaint(request: Request, complaint: ComplaintIn):
 async def delete_complaint(complaint_id: int):
     await ComplaintManager.delete_complaint(complaint_id)
 
+
+@router.put(
+    "/complaints/{complaint_id}/approve/",
+    dependencies=[Depends(oauth_scheme), Depends(is_approver)],
+    status_code=204
+)
+async def approve_complaint(complaint_id: int):
+    await ComplaintManager.approve(complaint_id)
+
+
+@router.put(
+    "/complaints/{complaint_id}/reject",
+    dependencies=[Depends(oauth_scheme), Depends(is_approver)],
+    status_code=204
+)
+async def reject_complaint(complaint_id: int):
+    await ComplaintManager.reject(complaint_id)
